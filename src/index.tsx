a40b40e501badf7c34a4e468f7bf572122e3f50a
Mahesh <66619868+mahesh-in@users.noreply.github.com>
2023-05-25 19:58:47 +0530

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import App from "./App";
+const client = new QueryClient();
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <QueryClientProvider client={client}>
+    <App />
+  </QueryClientProvider>,
+  rootElement
+);
